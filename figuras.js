dce9ff7348c3a969bbdb78db6d3a2bc7758d9e7a
Mario Almengor <mario24elias@gmail.com>
2022-01-04 22:00:32 -0600

    Agregado calculo para la altura de un isosceles

--- figuras.js
+++ figuras.js
@@ -70,3 +70,33 @@ function calcularAreaCuadrado() {
     alert(area);
 
 }
+
+//Interactuando con el HTML para el triangulo
+function trianguloIso(){
+    let lado1 = document.getElementById("isoLado1");
+    let iLado1 = parseFloat(lado1.value);
+
+    let lado2 = document.getElementById("isoLado2");
+    let iLado2 = parseFloat(lado2.value);
+
+    let base = document.getElementById("isoBase");
+    let iBase = parseFloat(base.value);
+
+    if(iLado1 != iLado2){
+        alert("No se puede calcular debido a que los lados son diferentes");
+    }else{
+        const pequenoLado2 = iBase /2; 
+        const pequenoBase = iLado1;
+
+        const pequenoLado2Cuadrado = pequenoLado2 * pequenoLado2;
+        const pequenoBaseCuadrado = pequenoBase * pequenoBase;
+         
+        let pequenoLado1 = Math.sqrt(pequenoBaseCuadrado - pequenoLado2Cuadrado);
+
+        const grandeAltura = pequenoLado1;
+        alert(`La altura del triangulo es: ${grandeAltura}cm`);
+    }
+}
+
+
+
