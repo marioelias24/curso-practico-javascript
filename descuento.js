fb35c722b370035facb0d6268c837e3ee22a2c95
Mario Almengor <mario24elias@gmail.com>
2022-01-07 17:43:07 -0600

    Descuentos terminados

--- descuento.js
+++ descuento.js
@@ -1,15 +1,20 @@
 function calcularPrecioConDescuento(precio, descuento) {
-    return (precio * (100 - descuento)) / 100;
-}
-
-function onClickButtonPriceDiscount() {
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+  
+    return precioConDescuento;
+  }
+  
+  function onClickButtonPriceDiscount() {
     const inputPrice = document.getElementById("InputPrice");
-    const priceValue =inputPrice.value;
-
-    const InputDiscount = document.getElementById("InputDiscount");
-    const discountValue =InputDiscount.value;
-
-    const PrecioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
-
-}
-
+    const priceValue = inputPrice.value;
+    
+    const inputDiscount = document.getElementById("InputDiscount");
+    const discountValue = inputDiscount.value;
+  
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+  
+    const resultP = document.getElementById("ResultP");
+    resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+  }
+  
